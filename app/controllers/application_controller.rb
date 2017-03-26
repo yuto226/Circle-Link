586eae3812e82db8f1e1f8b467d7512b88f0788f
Yuto Suzuki <yuto.226.karasia@i.softbank.jp>
2017-03-26 22:01:57 +0900

    heroku Basic認証導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  http_basic_authenticate_with :name => 'admin', :password => 'hogehoge' if Rails.env == "production"
   protect_from_forgery with: :exception
 def after_sign_in_path_for resource
   admins_index_path
