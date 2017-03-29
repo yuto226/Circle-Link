e75336d57b771e8109519b021ff3d9821a9f52f1
Yamaguchi Satoshi <yama_sato3104@outlook.jp>
2017-03-29 21:51:14 +0900

    プロフィール変更ページのレイアウト

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -31,7 +31,7 @@ class HomesController < ApplicationController
 
       relation = relation.where(:genre_id => @conditions.genre_ids)
     end
-    @msg = relation.page(params[:page]).per(3)
+    @msg = relation.page(params[:page]).per(6)
   end
 
 end
