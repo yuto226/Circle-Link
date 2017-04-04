9f35ca65f983b458801c5447076da66aa5e5c312
Yuto Suzuki <yuto.226.karasia@i.softbank.jp>
2017-04-05 08:33:03 +0900

    記事バリデーション title 13から20へ 本文 200から300へ 応急処置です。

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ApplicationRecord
   belongs_to:admin
-  validates :title, presence: true,length: { maximum: 13 }
-  validates :contents, presence: true,length: { maximum: 200 }
+  validates :title, presence: true,length: { maximum: 20 }
+  validates :contents, presence: true,length: { maximum: 300 }
 end
