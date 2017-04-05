3b5f3a9f91a3d94db537e75fda5c43dcc1c87083
Yamaguchi Satoshi <yama_sato3104@outlook.jp>
2017-04-05 11:03:52 +0900

    新着情報のレイアウト微調整など

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ApplicationRecord
   belongs_to:admin
-  validates :title, presence: true,length: { maximum: 13 }
+  validates :title, presence: true,length: { maximum: 25 }
   validates :contents, presence: true,length: { maximum: 200 }
 end
