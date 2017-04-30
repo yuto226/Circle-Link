628a45c7e7cadffeb37d077f01a95d4e1d7f40e3
Yuto Suzuki <yuto.226.karasia@i.softbank.jp>
2017-04-30 22:01:00 +0900

    邪魔なmigrationファイル消しました

--- db/migrate/20170425135950_add_neet_id_to_neet_profs.rb
+++
@@ -1,5 +0,0 @@
-class AddNeetIdToNeetProfs < ActiveRecord::Migration[5.0]
-  def change
-    add_column :neet_profs, :neet_id, :integer
-  end
-end
--- db/migrate/20170426053822_add_janru_to_neet_profs.rb
+++
@@ -1,5 +0,0 @@
-class AddJanruToNeetProfs < ActiveRecord::Migration[5.0]
-  def change
-    add_column :neet_profs, :janru, :text
-  end
-end
